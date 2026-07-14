8b9ede8ef1ff218ea83ecb6bf687e2a48b70783e
Harrison Tin <harrison@palmier.io>
2026-07-13 21:40:20 -0700

    Fix session activation test assertions

--- Tests/PalmierProTests/Agent/AnalyticsSessionActivationTests.swift
+++ Tests/PalmierProTests/Agent/AnalyticsSessionActivationTests.swift
@@ -6,15 +6,19 @@ struct AnalyticsSessionActivationTests {
     @Test func capturesOnlyFirstActivation() {
         var activation = Analytics.SessionActivation()
 
-        #expect(activation.activate())
-        #expect(!activation.activate())
+        let firstActivation = activation.activate()
+        let secondActivation = activation.activate()
 
+        #expect(firstActivation)
+        #expect(!secondActivation)
         #expect(activation.isActivated)
     }
 
     @Test func restoredActiveSessionDoesNotCaptureAgain() {
         var activation = Analytics.SessionActivation(isActivated: true)
 
-        #expect(!activation.activate())
+        let repeatedActivation = activation.activate()
+
+        #expect(!repeatedActivation)
     }
 }
